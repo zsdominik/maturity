9cef160e6e82ddf8cf340dfa0f42d89821617f5e
Dominik Zsiros <Dominik_Zsiros@epam.com>
2019-11-26 14:50:06 +0100

    init

---
+++ src/test/java/com/zsirosd/maturity/MaturityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsirosd.maturity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaturityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

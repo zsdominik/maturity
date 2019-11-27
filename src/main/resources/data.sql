e3371bb7972de92fe7e3960ca81dc3cd349e96c0
Dominik Zsiros <Dominik_Zsiros@epam.com>
2019-11-27 14:56:00 +0100

    rename test movies

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into movies (movie_id, title, rating) values (1, 'Movie1', 5);
-insert into movies (movie_id, title, rating) values (2, 'Movie2', 8);
-insert into movies (movie_id, title, rating) values (3, 'Movie3', 7);
+insert into movies (movie_id, title, rating) values (1, 'Sherlock', 8);
+insert into movies (movie_id, title, rating) values (2, 'Hunger Games', 6);
+insert into movies (movie_id, title, rating) values (3, 'Avengers 3142', 7);
 
 insert into actors (actor_id, actor_name) values (1, 'Benedict Cumberbatch');
 insert into actors (actor_id, actor_name) values (2, 'Gérard Depardieu');
